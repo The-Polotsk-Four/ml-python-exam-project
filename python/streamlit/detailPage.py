fb4d8e92725057a261e24026f70157e396e4700b
Gustav <guwi0001@stud.kea.dk>
2026-05-05 11:24:46 +0200

    created the detail page file

---
+++ python/streamlit/detailPage.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+df = pd.read_csv('./ml-python-exam-project/machine_learning/resources/pokemon-dataset-gen-1-9/versions/10/pokeformslist.csv')
