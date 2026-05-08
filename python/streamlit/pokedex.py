6982f83b2714edbdb39c0a0f0f529833ec140984
Harning <hejthenerd@gmail.com>
2026-05-08 12:38:05 +0200

    added search functionality

--- python/streamlit/pokedex.py
+++ python/streamlit/pokedex.py
@@ -2,6 +2,7 @@ import requests
 import streamlit as st
 
 local_host = 'http://127.0.0.1:8000'
+pokemon_names = ['Show All Pokemon']
 
 res = requests.get(local_host + '/getAllPokemon')
 res_json = res.json()
@@ -17,16 +18,39 @@ st.set_page_config(
 st.title('Pokéd-Al')
 st.text('Welcome to the most advanced pokerdax on the web')
 
-for pokemon in res_json:
-    with st.container():
-        if pokemon['id'] <= 1025:
+def render_pokemon(given_pokemon):
+    pokemon_names.append(given_pokemon['name'])
+    with pokedex.container():
+        with poke_view.container():
             # st.write(pokemon)
-            if pokemon['sprite']:
-                st.image(pokemon['sprite'])
-                st.write(f'#{pokemon['id']:04d}')
-                st.write(pokemon['name'])
-                if len(pokemon['types']) == 1:
-                    st.write(pokemon['types'][0])
-                elif len(pokemon['types']) == 2:
-                    st.write(pokemon['types'][0] + '/' + pokemon['types'][1])
-                #st.write(len(pokemon['types']))
+            st.image(given_pokemon['sprite'])
+            st.write(f'#{given_pokemon['id']:04d}')
+            st.write(given_pokemon['name'])
+            if len(given_pokemon['types']) == 1:
+                st.write(given_pokemon['types'][0])
+            elif len(given_pokemon['types']) == 2:
+                st.write(given_pokemon['types'][0] + '/' + given_pokemon['types'][1])
+            # st.write(len(pokemon['types']))
+
+search_bar = st.container()
+pokedex = st.empty()
+poke_view = st.empty()
+
+for pokemon in res_json:
+    if pokemon['id'] <= 1025:
+        render_pokemon(pokemon)
+
+selected_pokemon = search_bar.selectbox('Choose a pokemon', pokemon_names)
+
+if selected_pokemon != 'Show All Pokemon':
+    pokedex.empty()
+    poke_view.empty()
+    for pokemon in res_json:
+        if pokemon['name'] == selected_pokemon:
+            render_pokemon(pokemon)
+if selected_pokemon == 'Show All Pokemon':
+    pokedex.empty()
+    poke_view.empty()
+    for pokemon in res_json:
+        if pokemon['id'] <= 1025:
+            render_pokemon(pokemon)
