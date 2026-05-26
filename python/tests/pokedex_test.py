8c0df0d1a8862e7d9d7221536d41ec275d0d888a
Gustav <guwi0001@stud.kea.dk>
2026-05-26 12:11:13 +0200

    Rettet nogle linjer til

--- python/tests/pokedex_test.py
+++ python/tests/pokedex_test.py
@@ -9,31 +9,20 @@ spec = importlib.util.spec_from_file_location("model_predict", module_path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 # Vi finder vores model i filerne og
-# loader den derefter med det udvalgte bibleotek
+# loader den derefter med det udvalgte bibliotek
 # Så vi kan bruge den i vores test
 
 
 predict_pokemon = module.predict_pokemon
 
-# TEST_IMAGE = "/machine_learning/roboflow_model/Pokemon-1/test/Eevee/00d762c891aa45b38173ce8bf9e74a4d_jpg.rf.69fc4a87cd57deabe307ea6f6d8b606b.jpg"
-TEST_IMAGE = r'python\tests\test_eevee.jpg'
-
+TEST_IMAGE = r'python/tests/test_eevee.jpg'
 # Ruten til billedet som vi bruger i vores test
-
-def test_pokemon_name_is_string():
-    name, score, image = predict_pokemon(TEST_IMAGE)
-    assert isinstance(name, str)
-    # Tester om navnet er en string
 
 def test_confidence_score_between_0_and_1():
     name, score, image = predict_pokemon(TEST_IMAGE)
     assert 0.0 <= score <= 1.0
     # Tjekker om scoren er mellem 0 og 1
 
-def test_image_is_numpy_array():
-    name, score, image = predict_pokemon(TEST_IMAGE)
-    assert isinstance(image, np.ndarray)
-    # Tester om billedet er i det rigtige format
 
 def test_pokemon_name_is_not_empty():
     name, score, image = predict_pokemon(TEST_IMAGE)
